58101d203e4f916d305fe3569b99ba400795f735
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-07 15:30:13 +0300

    added edit product route

--- e-commerce-v1/backend/server.js
+++ e-commerce-v1/backend/server.js
@@ -42,6 +42,23 @@ app.post("/products", async (req, res) => {
 	}
 })
 
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product Id" });
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+})
+
 app.delete("/products/:id", async (req, res) => {
 	const { id } = req.params;
 
