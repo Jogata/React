876c6c25e8788fe829b98f6235dc5b7d9ba95b86
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-07 22:21:21 +0300

    added product router

--- e-commerce-v1/backend/server.js
+++ e-commerce-v1/backend/server.js
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectBD } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -14,66 +13,7 @@ app.get("/", (req, res) => {
     res.send("Server is ready");
 })
 
-app.get("/products", async (req, res) => {
-    try {
-		const products = await Product.find({});
-		res.status(200).json({ success: true, data: products });
-	} catch (error) {
-		console.log("error in fetching products:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-})
-
-app.post("/products", async (req, res) => {
-    const product = req.body;
-
-    if (!product.name || !product.price || !product.image) {
-		return res.status(400).json({ success: false, message: "Please provide all fields" });
-	}
-
-	const newProduct = new Product(product);
-
-	try {
-		await newProduct.save();
-		res.status(201).json({ success: true, data: newProduct });
-	} catch (error) {
-		console.error("Error in Create product:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-})
-
-app.put("/products/:id", async (req, res) => {
-    const { id } = req.params;
-    
-    const product = req.body;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Invalid Product Id" });
-    }
-
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-        res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-})
-
-app.delete("/products/:id", async (req, res) => {
-	const { id } = req.params;
-
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-	}
-
-	try {
-		await Product.findByIdAndDelete(id);
-		res.status(200).json({ success: true, message: "Product deleted" });
-	} catch (error) {
-		console.log("error in Delete product:", error.message);
-		res.status(500).json({ success: false, message: "Server Error" });
-	}
-})
+app.use("/products", productRoutes);
 
 app.listen(5000, () => {
     connectBD();
