642eba19f42abb7f14c16760c538c1fbc44d56d9
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-09 20:06:19 +0300

    added theme mode

--- e-commerce-v1/frontend/src/components/Navigation.jsx
+++ e-commerce-v1/frontend/src/components/Navigation.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import "./navigation.css";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(true);
+
+    const icon = isDarkTheme ? "fa fa-sun-o" : "fa fa-moon-o";
+    const text = isDarkTheme ? "turn on light mode" : "turn on dark mode";
+
+    useEffect(() => {
+        console.log("use efffect");
+        if (isDarkTheme) {
+            document.body.classList.remove("light");
+            document.body.classList.add("dark");
+        } else {
+            document.body.classList.remove("dark");
+            document.body.classList.add("light");
+        }
+    }, [isDarkTheme])
+
 	return (
 		<nav>
-            {/* <Link to={"/"} className="logo">Product Store 🛒</Link> */}
             <Link to={"/"} className="logo">
                 Product Store
                 <i className="fa fa-shopping-cart"></i>
@@ -13,9 +29,9 @@ const Navigation = () => {
                 <Link>
                     <i className="fa fa-plus"></i>
                 </Link>
-                <button>
-                    toggle theme
-                    <i className="fa fa-sun-o"></i>
+                <button onClick={() => {setIsDarkTheme(!isDarkTheme)}}>
+                    {text}
+                    <i className={icon}></i>
                 </button>
             </div>
 		</nav>
