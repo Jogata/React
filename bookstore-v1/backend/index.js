8ca2bab85927e626202acc8bded23e03d4a1f416
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-11 13:40:35 +0300

    added get all books route

--- bookstore-v1/backend/index.js
+++ bookstore-v1/backend/index.js
@@ -1,29 +1,32 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from '../models/bookModel.js';
-// import booksRoute from "./routes/booksRoute.js";
-// import cors from "cors";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
+// app.get("/", (request, response) => {
+//     console.log(request);
+//     return response.status(234).send("Welcome");
+// });
 
-app.get("/", (request, response) => {
-    console.log(request);
-    return response.status(234).send("Welcome");
+app.get("/", async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books,
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
-router.post("/books", async (request, response) => {
+app.post("/books", async (request, response) => {
     try {
         if (
             !request.body.title ||
