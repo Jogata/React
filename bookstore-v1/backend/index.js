56cda933018d33055d56b786f629da201be6d2b2
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-11 11:31:11 +0300

    added create new book route

--- bookstore-v1/backend/index.js
+++ bookstore-v1/backend/index.js
@@ -2,14 +2,51 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from '../models/bookModel.js';
+// import booksRoute from "./routes/booksRoute.js";
+// import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
+// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
 app.get("/", (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome");
+});
+
+router.post("/books", async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
 // app.listen(PORT, () => {
