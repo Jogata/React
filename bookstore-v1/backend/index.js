fd309ce42ee33e7bff805e3e6e28181f52db176d
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-13 18:27:13 +0300

    added cors

--- bookstore-v1/backend/index.js
+++ bookstore-v1/backend/index.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
 import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
+
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.use(express.json());
 
