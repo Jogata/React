c4130aeaf834da2ce620a9d46be6b97f125b3be9
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-14 10:48:44 +0300

    added home page

--- bookstore-v1/frontend/src/App.jsx
+++ bookstore-v1/frontend/src/App.jsx
@@ -1,44 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function getAllBooks() {
-    fetch("http://localhost:5555/books")
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => getAllBooks()}>
-          get all books
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 style={{
+        padding: "5rem 1rem", 
+        color: "brown", 
+        fontSize: "5rem", 
+        textAlign: "center", 
+        textTransform: "capitalize", 
+      }}>test</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ bookstore-v1/frontend/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [showType, setShowType] = useState("table");
+
+    useEffect(() => {
+        setLoading(true);
+        fetch("http://localhost:5555/books")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setBooks(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            });
+    }, []);
+
+    return (
+        <div className="">
+            <div className="">
+                <button
+                    className=""
+                    onClick={() => setShowType("table")}
+                >
+                    Table
+                </button>
+                <button
+                    className=""
+                    onClick={() => setShowType("card")}
+                >
+                    Card
+                </button>
+            </div>
+            <div className="">
+                <h1>Books List</h1>
+                <Link to="/books/create">
+                    Create new book
+                </Link>
+            </div>
+            {loading ? (
+                <h2>loading</h2>
+            ) : (
+                <h2>TODO: map books</h2>
+            )}
+        </div>
+    );
+};
+
+export default Home;
