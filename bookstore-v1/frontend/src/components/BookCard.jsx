d0edad5efaa2fc8e8ee272c89421f31e20da3ec7
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-19 18:40:17 +0300

    added book card component

---
+++ bookstore-v1/frontend/src/components/BookCard.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+const BookCard = ({ book }) => {
+    return (
+        <div className="card">
+            <div className="card-row top-row">
+                <h2 className="year">
+                    {book.publishYear}
+                </h2>
+                <h4 className="book-id">{book._id}</h4>
+            </div>
+            <div className="card-row book-title">
+                <i className="fa fa-book"></i>
+                <h2>{book.title}</h2>
+            </div>
+            <div className="card-row author">
+                <i className="fa fa-user-o"></i>
+                <h2>{book.author}</h2>
+            </div>
+            <div className="actions">
+                <Link to={`/books/details/${book._id}`}>
+                    <i className="ri-information-line"></i>
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                    <i className="ri-edit-line"></i>
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                    <i className="fa fa-trash-o"></i>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
