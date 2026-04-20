554ff77b20df22d630667e6b0bed74d4ae6214f8
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-20 11:15:47 +0300

    updated book modal component

--- bookstore-v1/frontend/src/components/BookCard.jsx
+++ bookstore-v1/frontend/src/components/BookCard.jsx
@@ -6,42 +6,45 @@ const BookCard = ({ book }) => {
     const [showModal, setShowModal] = useState(false);
 
     return (
-        <div className="card">
-            <div className="card-row top-row">
-                <h2 className="year">
-                    {book.publishYear}
-                </h2>
-                <h4 className="book-id">{book._id}</h4>
-            </div>
-            <div className="card-row book-title">
-                <i className="fa fa-book"></i>
-                <h2>{book.title}</h2>
-            </div>
-            <div className="card-row author">
-                <i className="fa fa-user-o"></i>
-                <h2>{book.author}</h2>
-            </div>
-            <div className="actions">
-                <button onClick={() => setShowModal(true)}>
-                    show modal
-                </button>
-                <Link to={`/books/details/${book._id}`}>
-                    <i className="ri-information-line"></i>
-                </Link>
-                <Link to={`/books/edit/${book._id}`}>
-                    <i className="ri-edit-line"></i>
-                </Link>
-                <Link to={`/books/delete/${book._id}`}>
-                    <i className="fa fa-trash-o"></i>
-                </Link>
+        <>
+            <div className="card">
+                <div className="card-row top-row">
+                    <h2 className="year">
+                        {book.publishYear}
+                    </h2>
+                    <h4 className="book-id">{book._id}</h4>
+                </div>
+                <div className="card-row book-title">
+                    <i className="fa fa-book"></i>
+                    <h2>{book.title}</h2>
+                </div>
+                <div className="card-row author">
+                    <i className="fa fa-user-o"></i>
+                    <h2>{book.author}</h2>
+                </div>
+                <div className="actions">
+                    <button onClick={() => setShowModal(true)}>
+                        show modal
+                        <i className="fa fa-eye"></i>
+                    </button>
+                    <Link to={`/books/details/${book._id}`}>
+                        <i className="ri-information-line"></i>
+                    </Link>
+                    <Link to={`/books/edit/${book._id}`}>
+                        <i className="ri-edit-line"></i>
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                        <i className="fa fa-trash-o"></i>
+                    </Link>
+                </div>
             </div>
             {showModal && (
-                <BookModal 
-                    book={book} 
-                    onClose={() => setShowModal(false)} 
+                <BookModal
+                    book={book}
+                    onClose={() => setShowModal(false)}
                 />
             )}
-        </div>
+        </>
     );
 };
 
--- bookstore-v1/frontend/src/components/BookModal.jsx
+++ bookstore-v1/frontend/src/components/BookModal.jsx
@@ -1,15 +1,26 @@
+import { useEffect, useRef } from "react";
+
 const BookModal = ({ book, onClose }) => {
+    const focusPoint = useRef(null);
+
+    useEffect(() => {
+        console.log(focusPoint.current);
+        focusPoint.current.focus();
+    }, [focusPoint.current])
+
     return (
         <div className="modal" onClick={onClose}>
             <div 
                 className="modal-box"
                 onClick={(event) => event.stopPropagation()} 
             >
-                <button onClick={onClose}>
-                    close
-                    <i className="fa fa-times"></i>
-                </button>
-                <h2>{book.publishYear}</h2>
+                <h2 
+                    className="year" 
+                    ref={focusPoint} 
+                    tabIndex="-1"
+                >
+                    {book.publishYear}
+                </h2>
                 <h4>{book._id}</h4>
                 <div className="row">
                     <i className="fa fa-book"></i>
@@ -28,6 +39,10 @@ const BookModal = ({ book, onClose }) => {
                     dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
                     vitae voluptate sequi repellat!
                 </p>
+                <button onClick={onClose}>
+                    close
+                    <i className="ri-close-line"></i>
+                </button>
             </div>
         </div>
     );
