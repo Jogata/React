a351b7342f2c7b0f101b4eb2a3cb2acc147d6570
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-26 18:34:36 +0300

    added create new user route

--- user-stories-v1/backend/controllers/userControllers.js
+++ user-stories-v1/backend/controllers/userControllers.js
@@ -14,6 +14,33 @@ const getAllUsers = async (req, res) => {
 
 const createNewUser = async (req, res) => {
     console.log("todo create");
+    try {        
+        const { username, password, roles } = req.body;
+    
+        if (!username || !password || !Array.isArray(roles) || !roles.length) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+    
+        const duplicate = await User.findOne({ username }).lean().exec();
+    
+        if (duplicate) {
+            return res.status(409).json({ message: "Duplicate username" });
+        }
+    
+        const hashedPwd = await bcrypt.hash(password, 10);
+    
+        const userObject = { username, "password": hashedPwd, roles };
+    
+        const user = await User.create(userObject)
+    
+        if (user) {
+            res.status(201).json({ message: `New user ${username} created` });
+        } else {
+            res.status(400).json({ message: "Invalid user data received" });
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 const updateUser = async (req, res) => {
