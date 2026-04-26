9f9a1d615caed0fa29119ef3a1aa203c018c4b3f
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-26 13:00:16 +0300

    added database

--- user-stories-v1/backend/server.js
+++ user-stories-v1/backend/server.js
@@ -1,16 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+// console.log(process.env.TEST);
 
+const app = express();
+connectDB();
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -27,4 +33,12 @@ app.use("/*splat", (req, res) => {
     }
 })
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+
+mongoose.connection.on("error", err => {
+    console.log(err);
+})
