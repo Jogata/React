ae16d44c40162b831b871d2cc88d853074be7ff9
Yonko Nikolov <jonkoslav@abv.bg>
2026-04-28 20:09:13 +0300

    added home page

--- user-stories-v1/frontend/src/App.jsx
+++ user-stories-v1/frontend/src/App.jsx
@@ -1,30 +1,12 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
-    </>
-  )
-}
-
-function Home() {
-  return (
-    <>
-      <h1>home</h1>
-      <Link to="/test">test</Link>
-    </>
-  )
-}
-
-function Test() {
-  return (
-    <>
-      <h1>test</h1>
-      <Link to="/">home</Link>
     </>
   )
 }
---
+++ user-stories-v1/frontend/src/components/Home.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    const content = (
+        <section className="home">
+            <header>
+                <h1>Welcome to <span className="nowrap">
+                    Dan D. Repairs!
+                </span></h1>
+            </header>
+            <main className="main">
+                <p className="main-desc">
+                    Located in Beautiful Downtown Foo City, 
+                    Dan D. Repairs  provides a trained staff 
+                    ready to meet your tech repair needs
+                </p>
+                <address className="addr">
+                    <span>Dan D. Repairs</span>
+                    <span>555 Foo Drive</span>
+                    <span>Foo City, CA 12345</span>
+                    <a href="tel:+15555555555">(555) 555-5555</a>
+                </address>
+                
+                <p>Owner: Dan Davidson</p>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+    )
+
+    return content;
+}
+
+export default Home;
