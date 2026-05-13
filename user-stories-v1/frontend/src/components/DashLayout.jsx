618f763f0bf6a036c647995619e8049ecdad47a9
Yonko Nikolov <jonkoslav@abv.bg>
2026-05-13 21:51:22 +0300

    updated dash layout component

--- user-stories-v1/frontend/src/components/DashLayout.jsx
+++ user-stories-v1/frontend/src/components/DashLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const DashLayout = () => {
     return (
@@ -21,11 +21,35 @@ const DashHeader = () => {
 }
 
 const DashFooter = () => {
-    return (
-        <footer>
-        <h1>DashFooter</h1>
+    const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    const onGoHomeClicked = () => navigate("/dash");
+
+    let goHomeButton = null;
+
+    if (pathname !== "/dash") {
+        goHomeButton = (
+            <button
+                className="dash-footer-button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                home
+                <i className="fa fa-home"></i>
+            </button>
+        )
+    }
+
+    const content = (
+        <footer className="dash-footer">
+            {goHomeButton}
+            <p>Current User:</p>
+            <p>Status:</p>
         </footer>
     )
+
+    return content;
 }
 
 export default DashLayout;
