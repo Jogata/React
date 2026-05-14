71b9e8f5b24d0fe902701980993d2e43f3fcdaf5
Yonko Nikolov <jonkoslav@abv.bg>
2026-05-14 12:37:00 +0300

    updated dash layout component

--- user-stories-v1/frontend/src/components/DashLayout.jsx
+++ user-stories-v1/frontend/src/components/DashLayout.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 const DashLayout = () => {
     return (
@@ -30,29 +30,64 @@ const DashHeader = () => {
 }
 
 const DashFooter = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { pathname } = useLocation();
 
-    const onGoHomeClicked = () => navigate("/dash");
+    // const onGoHomeClicked = () => navigate("/dash");
 
     let goHomeButton = null;
 
     if (pathname !== "/dash") {
         goHomeButton = (
-            <button
+            <Link 
+                to="/dash"
                 className="dash-footer-button icon-button"
                 title="Home"
-                onClick={onGoHomeClicked}
+                // onClick={onGoHomeClicked}
             >
                 home
                 <i className="fa fa-home"></i>
-            </button>
+            </Link>
         )
     }
 
     const content = (
         <footer className="dash-footer">
             {goHomeButton}
+            <p>Current User:</p>
+            <p>Status:</p>
+        </footer>
+    )
+
+    return content;
+}
+
+const WelcomeDashFooter = () => {
+    // const { pathname } = useLocation();
+
+    // let goHomeButton = null;
+    //     goHomeButton = (
+    //         <button
+    //             className="dash-footer-button icon-button"
+    //             title="Home"
+    //             onClick={onGoHomeClicked}
+    //         >
+    //             home
+    //             <i className="fa fa-home"></i>
+    //         </button>
+    //     )
+
+    const content = (
+        <footer className="dash-footer">
+            {/* <Link
+                to="/dash"
+                className="dash-footer-button icon-button"
+                title="Home"
+                onClick={onGoHomeClicked}
+            >
+                home
+                <i className="fa fa-home"></i>
+            </Link> */}
             <p>Current User:</p>
             <p>Status:</p>
         </footer>
