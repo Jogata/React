630a9e86b67dfcc3578c4e9321f7429df811d2e5
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-01 20:50:51 +0300

    practice

--- restaurant-reviews-v1/backend/api/restaurants.controller.js
+++ restaurant-reviews-v1/backend/api/restaurants.controller.js
@@ -1,6 +1,22 @@
 import RestaurantsDAO from "../dao/restaurantsDAO.js";
 
 export default class RestaurantsController {
+
+    static async apiGetRestaurantById(req, res, next) {
+        try {
+            let id = req.params.id || {};
+            let restaurant = await RestaurantsDAO.getRestaurantByID(id);
+            if (!restaurant) {
+                res.status(404).json({ error: "Not found" });
+                return;
+            }
+            res.json(restaurant);
+        } catch (err) {
+            console.log(`api, ${err}`);
+            res.status(500).json({ error: err });
+        }
+    }
+
     static async apiGetRestaurantCuisines(req, res, next) {
         try {
             let cuisines = await RestaurantsDAO.getCuisines();
