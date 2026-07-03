0672b5dea57ebefea39b8bcc6b880ac946e9bab2
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-03 13:27:39 +0300

    practice

--- restaurant-reviews-v1/backend/api/reviews.controller.js
+++ restaurant-reviews-v1/backend/api/reviews.controller.js
@@ -25,6 +25,36 @@ export default class ReviewsController {
         }
     }
 
+    static async apiUpdateReview(req, res, next) {
+        try {
+            const reviewId = req.body.review_id;
+            const text = req.body.text;
+            const date = new Date();
+
+            const reviewResponse = await ReviewsDAO.updateReview(
+                reviewId,
+                req.body.user_id,
+                text,
+                date,
+            );
+
+            var { error } = reviewResponse;
+
+            if (error) {
+                res.status(400).json({ error });
+            }
+
+            if (reviewResponse.modifiedCount === 0) {
+                throw new Error(
+                    "unable to update review - user may not be original poster",
+                );
+            }
+
+            res.json({ status: "success" });
+        } catch (err) {
+            res.status(500).json({ error: err.message });
+        }
+    }
 
     static async apiDeleteReview(req, res, next) {
         try {
