89fcdbe894bdf828b5c509976997c77e8201fc4c
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-04 13:58:01 +0300

    practice

--- restaurant-reviews-v1/frontend/src/App.jsx
+++ restaurant-reviews-v1/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   const [user, setUser] = useState(null);
