574a8b24ce154dcf3e334514a91f042fb72d264b
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-09 19:14:07 +0300

    practice

--- restaurant-reviews-v1/frontend/src/components/restaurant.jsx
+++ restaurant-reviews-v1/frontend/src/components/restaurant.jsx
@@ -3,15 +3,6 @@ import { Link, useParams } from "react-router-dom";
 import RestaurantDataService from "../services/restaurant";
 
 const Restaurant = () => {
-    // const initialRestaurantState = {
-    //     id: null,
-    //     name: "",
-    //     address: {},
-    //     cuisine: "",
-    //     reviews: [],
-    // };
-
-    // const [restaurant, setRestaurant] = useState(initialRestaurantState);
     const [restaurant, setRestaurant] = useState(null);
 
     const { id } = useParams();
@@ -19,9 +10,9 @@ const Restaurant = () => {
     const getRestaurant = (id) => {
         RestaurantDataService.getRestaurant(id)
             .then((response) => {
-                console.log(response);
+                // console.log(response);
 
-                if (response.ok) {                    
+                if (response.ok) {
                     const data = response.json();
                     return data;
                 } else {
@@ -44,99 +35,101 @@ const Restaurant = () => {
     return (
         <div>
             {restaurant ? (
-                <div className="card">
-                    <h1>{restaurant.name}</h1>
-                    {/* <p>
-                        <strong>Cuisine: </strong>
-                        {restaurant.cuisine}
-                        <br />
-                        <strong>Address: </strong>
-                        {restaurant.address.building} {restaurant.address.street},{" "}
-                        {restaurant.address.zipcode}
-                    </p> */}
-                    <dl>
-                        <div className="row">
-                            <dt>Cuisine:</dt>
-                            <dd>{restaurant.cuisine}</dd>
-                        </div>
-
-                        <div className="row">
-                            <dt>Address:</dt>
-                            <dd>
-                                {restaurant.address.building} {restaurant.address.street},{" "}
-                                {restaurant.address.zipcode}
-                            </dd>
-                        </div>
-                    </dl>
-                    <Link
-                        to={"/restaurants/" + id + "/review"}
-                        className="btn btn-primary"
-                    >
-                        Add Review
-                    </Link>
-                    <h4> Reviews </h4>
-                    <div className="row">
-                        {restaurant.reviews.length > 0 ? (
-                            restaurant.reviews.map((review, index) => {
-                                return (
-                                    <div key={index}>
-                                        <div className="card">
-                                            <div className="card-body">
-                                                <p className="card-text">
-                                                    {review.text}
-                                                    <br />
-                                                    <strong>User: </strong>
-                                                    {review.name}
-                                                    <br />
-                                                    <strong>Date: </strong>
-                                                    {review.date}
-                                                </p>
-                                                {props.user && props.user.id === review.user_id && (
-                                                    <div className="row">
-                                                        <a
-                                                            href="/#"
-                                                            onClick={() => deleteReview(review._id, index)}
-                                                            className="btn btn-primary col-lg-5 mx-1 mb-1"
-                                                        >
-                                                            Delete
-                                                        </a>
-                                                        <Link
-                                                            to={{
-                                                                pathname:
-                                                                    "/restaurants/" +
-                                                                    props.match.params.id +
-                                                                    "/review",
-                                                                state: {
-                                                                    currentReview: review,
-                                                                },
-                                                            }}
-                                                            className="btn"
-                                                        >
-                                                            Edit
-                                                        </Link>
-                                                    </div>
-                                                )}
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })
-                        ) : (
-                            <div>
-                                <p>No reviews yet.</p>
-                            </div>
-                        )}
-                    </div>
-                </div>
+                <Card restaurant={restaurant} />
             ) : (
-                <div>
-                    {/* <br /> */}
-                    {/* <p>No restaurant selected.</p> */}
-                    <h1>Restaurant not found</h1>
-                </div>
+                <NotFound />
             )}
         </div>
     );
 };
+
+function NotFound() {
+    return (
+        <div className="not-found-section">
+            <h1>Restaurant not found</h1>
+        </div>
+    )
+}
+
+function Card({ restaurant }) {
+    return (
+        <div className="restaurant-card">
+            <h1>{restaurant.name}</h1>
+            <dl>
+                <div className="row">
+                    <dt>Cuisine:</dt>
+                    <dd>{restaurant.cuisine}</dd>
+                </div>
+
+                <div className="row">
+                    <dt>Address:</dt>
+                    <dd>
+                        {restaurant.address.building} {restaurant.address.street},{" "}
+                        {restaurant.address.zipcode}
+                    </dd>
+                </div>
+            </dl>
+            <Link
+                to={"/restaurants/" + restaurant._id + "/review"}
+                className="btn btn-primary"
+            >
+                Add Review
+            </Link>
+            <h4> Reviews </h4>
+            <div className="row">
+                {restaurant.reviews.length > 0 ? (
+                    restaurant.reviews.map((review, index) => {
+                        return (
+                            <div key={index}>
+                                <div className="card">
+                                    <div className="card-body">
+                                        <p className="card-text">
+                                            {review.text}
+                                            <br />
+                                            <strong>User: </strong>
+                                            {review.name}
+                                            <br />
+                                            <strong>Date: </strong>
+                                            {review.date}
+                                        </p>
+                                        {props.user && props.user.id === review.user_id && (
+                                            <div className="row">
+                                                <a
+                                                    href="/#"
+                                                    onClick={() => deleteReview(review._id, index)}
+                                                    className="btn btn-primary col-lg-5 mx-1 mb-1"
+                                                >
+                                                    Delete
+                                                </a>
+                                                <Link
+                                                    to={{
+                                                        pathname:
+                                                            "/restaurants/" +
+                                                            props.match.params.id +
+                                                            "/review",
+                                                        state: {
+                                                            currentReview: review,
+                                                        },
+                                                    }}
+                                                    className="btn"
+                                                >
+                                                    Edit
+                                                </Link>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })
+                ) : (
+                    <div>
+                        <p>No reviews yet.</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
 
 export default Restaurant;
