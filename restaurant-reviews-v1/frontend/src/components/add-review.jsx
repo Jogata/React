0e3a5da9aad43c40a1ae9cc47d7d9b155494901d
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-13 16:49:14 +0300

    practice

--- restaurant-reviews-v1/frontend/src/components/add-review.jsx
+++ restaurant-reviews-v1/frontend/src/components/add-review.jsx
@@ -7,20 +7,21 @@ const AddReview = ({ user }) => {
     const { state } = useLocation();
     console.log(state);
     // console.log(restaurantId);
-    // let user = {};
     let submitted = true;
     const editing = Boolean(state?.currentReview);
-    // let editing = false;
 
-    // if (state) {
-    //     if (state.currentReview) {
-    //         editing = true;
-    //     }
-    // }
+    if (!user) {
+        return (
+            <div className="message-section">
+                <h1>Please log in.</h1>
+                <Link to="/login">Login</Link>
+            </div>
+        );
+    }
 
-    // if (state && state.currentReview) {
-    //         editing = true;
-    // }
+    const handleInputChange = (event) => {
+        setReview(event.target.value);
+    };
 
     if (submitted) {
         return <SubmitSuccess restaurantId={restaurantId} />;
@@ -32,59 +33,30 @@ const AddReview = ({ user }) => {
                 <div className="submit-form">
                     {submitted ? (
                         <SubmitSuccess restaurantId={restaurantId} />
-                        // <div>
-                        //     <h4>You submitted successfully!</h4>
-                        //     <Link
-                        //         to={"/restaurants/" + restaurantId}
-                        //         className="btn btn-success"
-                        //     >
-                        //         Back to Restaurant
-                        //     </Link>
-                        // </div>
                     ) : (
-                        <div>
-                            {/* <span className="flex items-center gap-2">
-            <svg 
-                className="animate-spin h-5 w-5 text-white" 
-        xmlns="http://w3.org" 
-        fill="none" 
-        viewBox="0 0 24 24">
-        <circle 
-          className="opacity-25" 
-          cx="12" 
-          cy="12" 
-          r="10" 
-          stroke="currentColor" 
-          strokeWidth="4"
-        />
-        <path 
-          className="opacity-75" 
-          fill="currentColor" 
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        />
-        </svg> 
-        Saving Review...
-          </span> */}
-                            <div className="form-group">
-                                <label htmlFor="description">
-                                    {editing ? "Edit" : "Create"} Review
-                                </label>
-                                <input
-                                    type="text"
-                                    id="text"
-                                    name="text"
-                                    className="form-control"
-                                    required
-                                />
-                            </div>
-                            <button className="btn btn-primary">
-                                Submit
-                            </button>
-                        </div>
+                        <ReviewInputForm editing={editing} />
+                        // <div>
+                        //     <div className="form-group">
+                        //         <label htmlFor="description">
+                        //             {editing ? "Edit" : "Create"} Review
+                        //         </label>
+                        //         <input
+                        //             type="text"
+                        //             id="text"
+                        //             name="text"
+                        //             className="form-control"
+                        //             required
+                        //         />
+                        //     </div>
+                        //     <button className="btn btn-primary">
+                        //         Submit
+                        //     </button>
+                        // </div>
                     )}
                 </div>
             ) : (
-                <div>Please log in.</div>
+                // <div>Please log in.</div>
+                null
             )}
         </div>
     );
@@ -92,7 +64,7 @@ const AddReview = ({ user }) => {
 
 function SubmitSuccess({ restaurantId }) {
     return (
-        <div className="response-section">
+        <div className="message-section">
             <h1>You submitted successfully!</h1>
             <Link
                 to={`/restaurants/${restaurantId}`}
@@ -100,6 +72,32 @@ function SubmitSuccess({ restaurantId }) {
                 Back to Restaurant
             </Link>
         </div>
+    );
+}
+
+function ReviewInputForm({ editing }) {
+    function onSubmit() {
+        console.log("todo create review");
+    }
+
+    return (
+        <form onSubmit={onSubmit} className="submit-form">
+            <div className="flex flex-col gap-1">
+                <label htmlFor="description">
+                    {editing ? "Edit" : "Create"} Review
+                </label>
+                <input
+                    type="text"
+                    id="description"
+                    name="description"
+                    className="form-control"
+                    required
+                />
+            </div>
+            <button type="submit" className="btn btn-primary">
+                Submit
+            </button>
+        </form>
     );
 }
 
