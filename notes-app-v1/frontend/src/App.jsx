43dd39373a154d2717d0fbaf74625780bac6d1c2
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-22 14:02:37 +0300

    practice

--- notes-app-v1/frontend/src/App.jsx
+++ notes-app-v1/frontend/src/App.jsx
@@ -1,10 +1,15 @@
 function App() {
   return (
-    <div className="test">
-      <nav>navbar</nav>
+    <div className="page">
+      <nav>
+        <h2>navbar</h2>
+      </nav>
       <main>
         <h1>test</h1>
       </main>
+      <footer>
+        <h2>footer</h2>
+      </footer>
     </div>
   )
 }
