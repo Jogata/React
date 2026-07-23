18d8f9ab5dd674f11542112210709916dd2e03f3
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-23 11:09:22 +0300

    practice

--- notes-app-v1/backend/controllers/product.controller.js
+++ notes-app-v1/backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
 export const getProducts = async (req, res) => {
@@ -24,6 +25,23 @@ export const createProduct = async (req, res) => {
 		res.status(201).json({ success: true, data: newProduct });
 	} catch (error) {
 		console.error("Error in Create product:", error.message);
+		res.status(500).json({ success: false, message: "Server Error" });
+	}
+};
+
+export const updateProduct = async (req, res) => {
+	const { id } = req.params;
+
+	const product = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ success: false, message: "Invalid Product Id" });
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+		res.status(200).json({ success: true, data: updatedProduct });
+	} catch (error) {
 		res.status(500).json({ success: false, message: "Server Error" });
 	}
 };
