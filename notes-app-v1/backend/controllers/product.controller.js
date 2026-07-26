8f7357b5ed4bdd4edff3bb282744e69c15919f81
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-26 17:24:15 +0300

    practice

--- notes-app-v1/backend/controllers/product.controller.js
+++ notes-app-v1/backend/controllers/product.controller.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
 export const getProducts = async (req, res) => {
 	try {
 		const products = await Product.find({});
+		// const sortedProducts = await Product.find({}).sort({createdAt: -1});
 		res.status(200).json({ success: true, data: products });
-		// res.status(200).send("test message");
+		// res.status(200).send("text message");
 	} catch (error) {
 		console.log("error in fetching products:", error.message);
 		res.status(500).json({ success: false, message: "Server Error" });
@@ -32,9 +33,9 @@ export const updateProduct = async (req, res) => {
 
 	const product = req.body;
 
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-	}
+	// if (!mongoose.Types.ObjectId.isValid(id)) {
+	// 	return res.status(404).json({ success: false, message: "Invalid Product Id" });
+	// }
 
 	try {
 		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
@@ -47,9 +48,9 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
 	const { id } = req.params;
 
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ success: false, message: "Invalid Product Id" });
-	}
+	// if (!mongoose.Types.ObjectId.isValid(id)) {
+	// 	return res.status(404).json({ success: false, message: "Invalid Product Id" });
+	// }
 
 	try {
 		await Product.findByIdAndDelete(id);
--- notes-app-v1/frontend/src/components/pages/Home.jsx
+++ notes-app-v1/frontend/src/components/pages/Home.jsx
@@ -1,15 +1,15 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
     const [products, setProducts] = useState(null);
     const [error, setError] = useState(null);
 
-	useEffect(() => {
+    useEffect(() => {
         console.log("home use effect");
         getAllProducts();
 
-		async function getAllProducts() {
+        async function getAllProducts() {
             const response = await fetch("http://localhost:5000/api/products");
             console.log(response);
 
@@ -20,11 +20,12 @@ const HomePage = () => {
             const contentType = response.headers.get("content-type");
             console.log(contentType);
             let result = null;
-         
+
             if (contentType && contentType.includes("application/json")) {
-              result = await response.json();
-              console.log(result);
-              setProducts(result.data);
+                result = await response.json();
+                console.log(result);
+                setProducts(result.data);
+                // setProducts([]);
             } else {
                 result = await response.text();
                 setError(result);
@@ -32,9 +33,9 @@ const HomePage = () => {
 
             console.log(result);
         }
-	}, []);
+    }, []);
 
-	console.log("products", products);
+    console.log("products", products);
 
     if (error) {
         return (
@@ -48,9 +49,32 @@ const HomePage = () => {
         )
     }
 
-	return (
-        <h1>{products[0].name}</h1>
-	);
+    return (
+        // <h1>{products[0].name}</h1>
+        <>
+            {products.length === 0 ? (
+                <header className="main-header">
+                    <h1>No products found</h1>
+                    <Link to="/create">
+                        Create a product
+                    </Link>
+                </header>
+            ) : (
+                <>
+                    <header className="main-header">
+                        <h1>Current Products</h1>
+                    </header>
+                    <div className="main-body">
+                        <section className="main-section">
+                            {products.map(product => (
+                                <h2 key={product._id}>{product.name}</h2>
+                            ))}
+                        </section>
+                    </div>
+                </>
+            )}
+        </>
+    );
 };
 
 export default HomePage;
