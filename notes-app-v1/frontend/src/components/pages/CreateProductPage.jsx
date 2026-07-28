99c98fc002bcdb3f75bc3c879471dc9e135ec22b
Yonko Nikolov <jonkoslav@abv.bg>
2026-07-28 14:03:52 +0300

    practice

--- notes-app-v1/frontend/src/components/pages/CreateProductPage.jsx
+++ notes-app-v1/frontend/src/components/pages/CreateProductPage.jsx
@@ -7,24 +7,27 @@ function CreateProductPage() {
         image: "https://cdn.pixabay.com/photo/2016/11/21/13/58/analog-watch-1845547_1280.jpg",
     });
 
-    async function createProduct() {
+    async function createProduct(e) {
+        e.preventDefault();
         console.log("todo create product");
     }
 
     return (
         <>
-            <header className="page-header">
+            <header className="main-header">
                 <h1>Create New Product</h1>
             </header>
 
-            <form>
+            <form className="form centered" onSubmit={createProduct}>
                 <input
+                    className="form-input"
                     name="name"
                     value={newProduct.name}
                     onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
                     placeholder="Product Name"
                 />
                 <input
+                    className="form-input"
                     type="number"
                     name="price"
                     value={newProduct.price}
@@ -32,13 +35,14 @@ function CreateProductPage() {
                     placeholder="Price"
                 />
                 <input
+                    className="form-input"
                     name="image"
                     value={newProduct.image}
                     onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
                     placeholder="Image URL"
                 />
 
-                <button onClick={createProduct}>
+                <button className="btn">
                     Add Product
                 </button>
             </form>
