fb46924a32024c1a104e8fbc2f8e9b57c36020b1
Yonko Nikolov <jonkoslav@abv.bg>
2026-08-20 20:56:14 +0300

    practice

--- notes-app-v1/frontend/src/components/pages/Home.jsx
+++ notes-app-v1/frontend/src/components/pages/Home.jsx
@@ -251,7 +251,7 @@ function ProductsSection({ products, handleDeleteProduct, handleUpdateProduct })
                 <section className="section">
                     <div className="products">
                         {products.map(product => (
-                            <ProductCardLink
+                            <ProductCardAsLink
                                 key={product._id} 
                                 product={product} 
                                 handleDeleteProduct={handleDeleteProduct} 
@@ -382,6 +382,80 @@ function ProductCardLink({ product, handleDeleteProduct, handleUpdateProduct }) 
     );
 }
 
+function ProductCardAsLink({ product, handleDeleteProduct }) {
+    const linkRef = useRef(null);
+
+    function openProductPage(e) {
+        // e.preventDefault();
+        linkRef.current.click();
+        console.log("clicked");
+    }
+
+    function handleDeleteProductWithStop(e, pid) {
+        e.stopPropagation();
+        handleDeleteProduct(pid);
+    }
+
+    return (
+        <article className="product-card" onClick={openProductPage}>
+        <img src={product.image} alt={product.name} />
+
+        <div className="content">
+            <h2>
+                <Link to={`/products/${product._id}`} className="main-card-link1" ref={linkRef}>
+                    {product.name}
+                </Link>
+            </h2>
+            <data className="price" value={product.price}>${product.price}</data>
+            <p onClick={e => e.stopPropagation()}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet eos labore, ratione similique error culpa. Voluptates corrupti quisquam accusantium quasi omnis, nisi numquam nihil itaque eos atque alias voluptatibus molestiae soluta possimus consequatur dolores repudiandae, aut rem! Quidem dolores libero ipsum, rem eos eveniet ea praesentium voluptatem magnam esse itaque.</p>
+        </div>
+
+        <div className="actions">
+            <button
+                type="button"
+                className="icon edit-btn"
+                title="Edit"
+            >
+                <span className="sr-only">Edit product {product.name}</span>
+                <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+            </button>
+            <button
+                type="button"
+                className="icon delete-btn"
+                title="Delete"
+                onClick={(e) => handleDeleteProductWithStop(e, product._id)}
+            >
+                <span className="sr-only">Delete product {product.name}</span>
+                <i className="fa fa-trash-o" aria-hidden="true"></i>
+            </button>
+        </div>
+
+        <footer className="metadata">
+            <p>{formatDate(new Date(product.createdAt))}</p>
+        </footer>
+    </article>
+
+        // <div className="product-card" onClick={openProductPage}>
+
+        //     <img src="shirt.jpg" alt="" />
+
+        //     <h3>
+        //         <Link ref={linkRef} to="/products/1">Blue Shirt</Link>
+        //     </h3>
+
+        //     <p>Our best selling shirt.</p>
+
+        //     <button
+        //         type="button"
+        //         className="btn"
+        //         title="Buy product"
+        //         onClick={() => handleBuyProduct(product._id)}
+        //     >
+        //         Buy product
+        //     </button>
+        // </div>
+    );
+}
 
 function TestCardAsLink() {
     return (
