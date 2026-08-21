fcb3ca54d10bf002ef7b6b97fb3e835fd5c4f2c6
Yonko Nikolov <jonkoslav@abv.bg>
2026-08-21 21:45:26 +0300

    practice

--- notes-app-v1/frontend/src/components/pages/Home.jsx
+++ notes-app-v1/frontend/src/components/pages/Home.jsx
@@ -476,6 +476,48 @@ function ProductCardAsLink({ product, handleDeleteProduct }) {
     );
 }
 
+function Modal({ isModalOpen, setModalMode, onClose, title, children }) {
+    const dialogRef = useRef(null);
+
+    useEffect(() => {
+        const dialogNode = dialogRef.current;
+        if (!dialogNode) return;
+
+        if (isModalOpen) {
+            dialogNode.showModal();
+            setModalMode(true);
+        } else {
+            dialogNode.close();
+            setModalMode(false);
+        }
+
+        return () => {
+            setModalMode(false);
+        };
+    }, [isModalOpen]);
+
+    return (
+        <dialog
+            className="modal"
+            ref={dialogRef}
+            onClose={onClose}
+            onClick={onClose}
+        >
+            <header>
+                <button type="button" className="icon" onClick={onClose}>
+                    <span className="sr-only">Close Modal</span>
+                    <i className="fa fa-times" aria-hidden="true"></i>
+                </button>
+                <h2 id="modal-title">{title}</h2>
+            </header>
+
+            <div className="modal-body" onClick={e => e.stopPropagation()}>
+                {children}
+            </div>
+        </dialog>
+    );
+}
+
 function TestCardAsLink() {
     return (
         <div data-src-id="1" data-src-fp="false" data-crb-el="uBWDxb" data-crb-gbl="false" decode-data-ved="1" className="cRH23c" data-ved="0CAAQqIcPahcKEwjA5urdtqyWAxUAAAAAHQAAAAAQLg" data-hveid="0">
