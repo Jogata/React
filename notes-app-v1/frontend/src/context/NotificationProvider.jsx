1f94280e3718f02ef0f2e6bbb17ae7731e560a09
Yonko Nikolov <jonkoslav@abv.bg>
2026-08-24 16:23:17 +0300

    practice

--- notes-app-v1/frontend/src/context/NotificationProvider.jsx
+++ notes-app-v1/frontend/src/context/NotificationProvider.jsx
@@ -27,34 +27,4 @@ export function NotificationProvider({ children }) {
     );
 }
 
-function Notification({ toast, onDismiss }) {
-  const [fadeout, setFadeout] = useState(false)
-  const id = toast.id;
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setFadeout(true);
-    }, 6000);
-
-    return () => clearTimeout(timer);
-  }, [onDismiss, id]);
-
-  // const accessibilityRole = toast.type === "error" ? "alert" : "status";
-  const notificationClassName = fadeout ? `toast-box ${toast.type} fade-out` : `toast-box ${toast.type}`;
-
-  return (
-    // <div className={`toast-box ${toast.type}`} role={accessibilityRole}>
-    <div className={notificationClassName} onAnimationEnd={() => onDismiss(id)}>
-      <p>{toast.message}</p>
-      <button
-        type="button"
-        onClick={() => onDismiss(id)}
-        aria-label="Dismiss alert"
-      >
-        <span>X</span>
-      </button>
-    </div>
-  );
-}
-
 export const useNotify = () => useContext(NotificationContext);
