46bd88b70bbb8ab644531defa6bd6be74eadccf0
Yonko Nikolov <jonkoslav@abv.bg>
2026-08-28 18:52:29 +0300

    practice

--- notes-think-board/backend/controllers/notesController.js
+++ notes-think-board/backend/controllers/notesController.js
@@ -20,3 +20,16 @@ export async function getNoteById(req, res) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export async function createNote(req, res) {
+    try {
+        const { title, content } = req.body;
+        const note = new Note({ title, content });
+
+        const savedNote = await note.save();
+        res.status(201).json(savedNote);
+    } catch (error) {
+        console.error("Error in createNote controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
