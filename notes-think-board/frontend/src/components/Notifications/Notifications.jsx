85b1c86f2f15885a4d453b6e9e48f4f3bc88cf19
Yonko Nikolov <jonkoslav@abv.bg>
2026-09-17 12:20:42 +0300

    practice

--- notes-think-board/frontend/src/components/Notifications/Notifications.jsx
+++ notes-think-board/frontend/src/components/Notifications/Notifications.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { Notification } from "./Notification";
+import "./notifications.css";
 
 export function Notifications({ notifications, removeNotification }) {
     const popoverRef = useRef(null);
