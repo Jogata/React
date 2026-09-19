5ceab311a85bfb792530ccddee5a6f819928bb40
Yonko Nikolov <jonkoslav@abv.bg>
2026-09-19 10:31:47 +0300

    practice

--- notes-think-board/frontend/src/services/notes.js
+++ notes-think-board/frontend/src/services/notes.js
@@ -20,6 +20,29 @@ async function getAllNotes() {
     }
 }
 
+async function getNoteByID(id) {
+    console.log(id);
+    const response = await fetch(`http://localhost:5000/api/notes/${id}`, {
+        signal: controller.signal
+    });
+
+    const contentType = response.headers.get("content-type");
+    let result = null;
+
+    if (contentType && contentType.includes("application/json")) {
+        result = await response.json();
+    } else {
+        result = await response.text();
+    }
+
+    if (response.ok) {
+        return result;
+    } else {
+        const errorMessage = result.message || "An error occurred";
+        throw new Error(errorMessage);
+    }
+}
+
 export const notesApi = {
     getAllNotes, 
     getNoteByID, 
