6b2d62cee0f5796223c0eec1f78beada6bb8ccd0
Yonko Nikolov <jonkoslav@abv.bg>
2026-09-20 16:01:38 +0300

    practice

--- notes-think-board/frontend/src/context/NotesProvider.jsx
+++ notes-think-board/frontend/src/context/NotesProvider.jsx
@@ -4,9 +4,34 @@ const NotesContext = createContext(null);
 
 export function NotesProvider({ children }) {
     const [notes, setNotes] = useState(null);
-        
+
+    function initializeNotes(notes) {
+        setNotes(notes);
+    }
+
+    function addNote(note) {
+        setNotes(old => notes == null ? null : [...old, note]);
+    }
+
+    function updateNote(id) {
+        setNotes(old => old.map(note => note._id !== id));
+    }
+
+    function deleteNote(id) {
+        setNotes(old => old.filter(note => note._id !== id));
+    }
+
+    const context = { 
+        notes, 
+        // setNotes, 
+        initializeNotes, 
+        addNote, 
+        updateNote, 
+        deleteNote 
+    };
+
     return (
-        <NotesContext.Provider value={{ notes, setNotes }}>
+        <NotesContext.Provider value={{ context }}>
             {children}
         </NotesContext.Provider>
     );
