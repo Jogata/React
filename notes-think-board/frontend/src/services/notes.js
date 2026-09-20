435e9ee674be9c2329a52d803bf7624d9ee495fd
Yonko Nikolov <jonkoslav@abv.bg>
2026-09-20 12:41:51 +0300

    practice

--- notes-think-board/frontend/src/services/notes.js
+++ notes-think-board/frontend/src/services/notes.js
@@ -101,6 +101,29 @@ async function updateNote(updatedNote) {
     }
 }
 
+async function deleteNote(id) {
+    const response = await fetch(
+        `http://localhost:5000/api/notes/${id}`, {
+            method: "DELETE"
+        });
+
+    const contentType = response.headers.get("content-type");
+    let result = null;
+
+    if (contentType && contentType.includes("application/json")) {
+        result = await response.json();
+    } else {
+        result = await response.text();
+    }
+
+    if (response.ok) {
+        return result;
+    } else {
+        const errorMessage = result.message || "An error occurred";
+        throw new Error(errorMessage);
+    }
+}
+
 export const notesApi = {
     getAllNotes, 
     getNoteByID, 
